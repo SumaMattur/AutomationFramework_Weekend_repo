349d720644ab6f222e92fe237a996bfd009202cf
SUMA <SUMA@DESKTOP-458OALL>
2025-04-05 16:04:50 +0530

    First New commit

---
+++ AutomationFramework_Weekend/src/test/java/GitHubTest/SumaDemo.java
@@ -0,0 +1,14 @@
+package GitHubTest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SumaDemo {
+	@Test
+	
+	public void Test() {
+	
+	Reporter.log("Adding new Feature");
+	
+	}
+}
